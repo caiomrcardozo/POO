a0189b0f4d6f13a156a11992a389d6280055810c
Caio Cardozo <162453676+caiomrcardozo@users.noreply.github.com>
2024-03-20 09:13:18 -0300

    Herança
    Aula dia 20/03 - Exercícios feitos

---
+++ Classe_Filha_1.java
@@ -0,0 +1,29 @@
+package Herança;
+
+public class Classe_Filha_1 extends Superclasse {
+	
+	private String atributo_2;
+	private String atributo_3;
+	
+	Classe_Filha_1(){
+		super();
+		System.out.println("acesso ao método construtor da Classe_Filha_1");
+	}
+	
+	public void set_atributo_2(String atributo_2) {
+		this.atributo_2 = atributo_2;
+	}
+	
+	public void set_atributo_3(String atributo_3) {
+		this.atributo_3 = atributo_3;
+	}
+	
+	public String get_atributo_2() {
+		return atributo_2;
+	}
+	
+	public String get_atributo_3() {
+		return atributo_3;
+	}
+
+}
